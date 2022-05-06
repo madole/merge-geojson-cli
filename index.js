5f512aa8a773eee4c70d69947575f618efc9949a
Andrew McDowell <madoliole@gmail.com>
2022-05-06 10:53:05 +1000

    FIRST

---
+++ index.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import { featureCollection } from "@turf/helpers";
+
+if (process.argv.length <= 2) {
+  console.log("Usage: node index.js -o <outfile> <srcfile> ...<srcfile>");
+  process.exit(1);
+}
+const outputFile = process.argv[process.argv.indexOf("-o") + 1];
+
+const sourceFiles = process.argv
+  .slice(4)
+  .filter((file) => file.endsWith(".geojson"));
+
+const sourceData = sourceFiles
+  .map((file) => JSON.parse(fs.readFileSync(file)))
+  .map(({ features }) => features)
+  .flat();
+
+const combined = featureCollection(sourceData);
+
+fs.writeFileSync(outputFile, JSON.stringify(combined, null, 2), { flag: "w+" });
